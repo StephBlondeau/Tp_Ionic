996c178f87df69c55c8e2f869f59477ebba3567b
imadhy <imadhy.gt@gmail.com>
2017-12-20 15:57:10 +0200

    Added searchbar to personnage page to filter characters based on user input

--- src/pages/personnage/personnage.ts
+++ src/pages/personnage/personnage.ts
@@ -3,13 +3,8 @@ import { IonicPage, NavController } from 'ionic-angular';
 import { PersonDetailsPage } from '../person-details/person-details';
 import { SWassocService } from './../../services/sw-assocService.service';
 import { People } from '../../models/people.model';
-
-/**
- * Generated class for the PersonnagePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
 
 @IonicPage()
 @Component({
@@ -19,23 +14,65 @@ import { People } from '../../models/people.model';
 export class PersonnagePage {
 
   listPeople: People[];
+  searchTerm: string = '';
+  searchControl: FormControl;
+  searching: any = false;
 
 
   constructor(
     public navCtrl: NavController,
     public _SWassocService: SWassocService) {
 
+    this.searchControl = new FormControl();
     this.initializeItems();
 
   }
 
+  ionViewDidLoad() {
+
+    this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+      this.searching = false;
+      this.setFilteredItems(search);
+    });
+  }
+
   initializeItems() {
-    this._SWassocService.getAllPeople().subscribe((data) => {
-        this.listPeople = data;
-      },
+    this._SWassocService.getAllPeople().subscribe(
+      data => this.listPeople = data,
       error => console.log(error));
   }
 
+  onSearchInput() {
+    this.searching = true;
+  }
+
+  setFilteredItems(searchTerm) {
+
+    if (searchTerm) {
+      this.filterCharacters(searchTerm);
+    } else {
+      this.initializeItems();
+    }
+  }
+
+  filterCharacters(value) {
+    // if the value is an empty string don't filter the items
+    if (!value) {
+      return;
+    }
+
+    this.listPeople = this.listPeople.filter((v) => {
+      if (v.name && value) {
+        if (v.name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+    console.log(value, this.listPeople.length);
+
+  }
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
