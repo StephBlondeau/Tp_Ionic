56fca17dd112e69901a1cd61d4f97511e84ab0de
imadhy <imadhy.gt@gmail.com>
2017-12-21 13:56:38 +0200

    Fix app module for prod release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { MovieDetailsPage } from './../pages/movie-details/movie-details';
+import { SplashPageModule } from './../pages/splash/splash.module';
+import { AssociationPageModule } from './../pages/association/association.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { PersonnagePage } from '../pages/personnage/personnage';
-import { AssociationPage } from '../pages/association/association';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { SplashPage } from '../pages/splash/splash';
 
 // Services
 import { themoviedbService } from '../services/themoviedbService.service';
@@ -21,34 +19,30 @@ import { MovieDetailsPageModule } from '../pages/movie-details/movie-details.mod
 
 import { Ionic2RatingModule } from "ionic2-rating";
 import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
-import { PersonDetailsPage } from '../pages/person-details/person-details';
 import { EmailComposer } from '@ionic-native/email-composer';
+import { PersonnagePageModule } from '../pages/personnage/personnage.module';
+import { PersonDetailsPageModule } from '../pages/person-details/person-details.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    PersonnagePage,
-    PersonDetailsPage,
-    SplashPage,
-    AssociationPage
+    HomePage
   ],
   imports: [
     HttpModule,
     BrowserModule,
     Ionic2RatingModule,
     MovieDetailsPageModule,
+    AssociationPageModule,
+    PersonDetailsPageModule,
+    PersonnagePageModule,
+    SplashPageModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    PersonnagePage,
-    PersonDetailsPage,
-    SplashPage,
-    MovieDetailsPage,
-    AssociationPage
+    HomePage
   ],
   providers: [
     themoviedbService,
